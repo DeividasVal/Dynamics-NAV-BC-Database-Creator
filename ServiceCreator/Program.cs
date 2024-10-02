4c41e142f91e3663d4a10b216084f24a3e5f7459
Deividas Valacka <deividas.valacka@simplanova.com>
2024-10-02 11:53:09 +0300

    Add project files.

---
+++ ServiceCreator/Program.cs
@@ -0,0 +1,12 @@
+namespace ServiceCreator
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
